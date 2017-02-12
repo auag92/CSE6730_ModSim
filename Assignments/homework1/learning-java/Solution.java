1c8aedcd8452b6ed508bb9d04bddb8a81c04209c
auag92 <apaar92@gmail.com>
2017-02-11 19:03:36 -0500

    java

---
+++ Assignments/homework1/learning-java/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution{
+
+  public static void main(String[] args) {
+    // System.out.println("Hello, World.");
+    // System.out.println("Hello, Java.");
+    Scanner s = new Scanner(System.in);
+
+    // String myString = scan.next();
+    int n = sc.nextInt();
+    if (n%2 == 0){
+      System.out.println("n > 20 and n is even, so it isn't weird. Thus, we print Not Weird.")
+    }
+  }
+}
---
+++ Assignments/homework1/learning-java/exec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# A simple script to run a java program
+
+javac $1.java
+java $1
