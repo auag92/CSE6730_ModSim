fee8f6fb32dd71d3e32f995c87c6fa6a7cf0c7c2
auag92 <apaar92@gmail.com>
2017-02-18 20:39:27 -0500

    think java book added

--- Assignments/homework1/learning-java/Solution.java
+++ Assignments/homework1/learning-java/Solution.java
@@ -6,11 +6,18 @@ public class Solution{
     // System.out.println("Hello, World.");
     // System.out.println("Hello, Java.");
     Scanner s = new Scanner(System.in);
-
-    // String myString = scan.next();
     int n = sc.nextInt();
-    if (n%2 == 0){
-      System.out.println("n > 20 and n is even, so it isn't weird. Thus, we print Not Weird.")
+    String ans;
+    // String myString = scan.next();
+    if (n%2 != 0){
+      ans = "Weird";
+    }
+    else {
+      if (n >= 2 && n <= 5){
+        ans = "Not Weird"
+      }
+      else if ()
+
     }
   }
 }
